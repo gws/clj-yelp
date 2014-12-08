b3680c65864c132239b208e48f7114d514b793bd
Gordon Stratton <gordon.stratton@gmail.com>
2014-12-08 04:53:29 +0000

    Bump dependencies and version and rename and relicense the project

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject gws/yelp "0.2.0"
+(defproject gws/clj-yelp "0.3.0"
   :description "A Clojure implementation of the Yelp API"
-  :url "https://gitlab.com/gws/yelp"
+  :url "https://gitlab.com/gws/clj-yelp"
   :min-lein-version "2.0.0"
-  :license {:name "ISC"
-            :url "http://opensource.org/licenses/ISC"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.4"]
+  :license {:name "Apache 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.scribe/scribe "1.3.5"]])
