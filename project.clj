ea3b411d07aa95fb7b6599302aebbbb0d7373f34
Gordon Stratton <gordon.stratton@gmail.com>
2015-08-22 20:48:46 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[cheshire "5.4.0"]
-                 [org.clojure/clojure "1.6.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]
+                   :global-vars {*warn-on-reflection* true}}}
+  :dependencies [[cheshire "5.5.0"]
                  [org.scribe/scribe "1.3.7"]])
