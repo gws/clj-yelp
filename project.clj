579e3810f20ee66e4fa2b317c31a8db1baf776d0
Gordon Stratton <gordon.stratton@gmail.com>
2016-03-02 20:29:15 +0000

    Add dev dependency on jonase/eastwood

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
+                   :plugins [[jonase/eastwood "0.2.3"]]
                    :global-vars {*warn-on-reflection* true}}}
   :dependencies [[cheshire "5.5.0"]
                  [org.scribe/scribe "1.3.7"]])
