975b9b9bda896eba0abb9aa126917a95b6b649a1
Gordon Stratton <gordon.stratton@gmail.com>
2016-08-12 20:17:42 +0000

    Move Clojure dependency to the :provided profile
    More detail:
    
    https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md#default-profiles

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
-                   :plugins [[jonase/eastwood "0.2.3"]]
-                   :global-vars {*warn-on-reflection* true}}}
   :dependencies [[cheshire "5.5.0"]
-                 [org.scribe/scribe "1.3.7"]])
+                 [org.scribe/scribe "1.3.7"]]
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]
+                   :global-vars {*warn-on-reflection* true}}
+             :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}})
